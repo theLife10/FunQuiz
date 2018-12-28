8f4fdc39850f79eb0ff10f8c4b21d53c9340d893
theLife10 <jdgarcia9@miners.utep.edu>
2018-12-28 02:23:17 -0700

    added listeners

--- app/src/main/java/com/fun/android/funquiz/MainActivity.java
+++ app/src/main/java/com/fun/android/funquiz/MainActivity.java
@@ -2,12 +2,40 @@ package com.fun.android.funquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton =(Button) findViewById(R.id.false_button);
+
+        //True button listener
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT)
+                .show();
+            }
+        });
+
+        //False button listener
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,R.string.false_toast,Toast.LENGTH_SHORT)
+                .show();
+            }
+        });
     }
 }
